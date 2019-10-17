82d8a52d7816062f2a5d143cac047c451a0f914a
Micah <micahincorporated@gmail.com>
2019-10-17 12:01:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+var optionTwo = {
+  strings: ["Hey, I'm Micah Lindley! ^1000"],
+  typeSpeed: 10
+};
+
+var typedAgain = new Typed('h1', optionTwo);
+
 var options = {
-  strings: ['designer ^1000', 'developer ^1000', 'musician ^1000', '^1000 :)'],
+  strings: ['^2000 designer ^1000', 'developer ^1000', 'musician ^1000', ':) ^1000'],
   typeSpeed: 40
 };
 
