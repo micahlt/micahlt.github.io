5826f6f3db20fc81dadeddd9a59744b354774431
Micah <micahincorporated@gmail.com>
2019-11-01 11:24:11 -0500

    Create youtube-music.js

---
+++ experiments/youtube-music.js
@@ -0,0 +1,28 @@
+let song = "https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3";
+document.getElementById('audiotag1').src = song;
+let audio = document.getElementById('audiotag1');
+let playOrPaused = "paused";
+audio.onloadedmetadata = function() {
+  alert(audio.duration);
+};
+/* Above is test code */
+function changePlayButton(){
+  if(document.getElementById("playbutton").className=="fas fa-play"){
+    document.getElementById("playbutton").className = "fa fa-pause";
+  }else{
+    document.getElementById("playbutton").className = "fa fa-play";
+  }
+}
+function changePlayState(){
+  if (playOrPaused == "paused") {
+    playOrPaused = "play";
+    changePlayButton();
+    document.getElementById('audiotag1').play();
+    console.log("state set to " + playOrPaused);
+  } else if (playOrPaused == "play") {
+    playOrPaused = "paused";
+    document.getElementById('audiotag1').pause();
+    console.log("state set to " + playOrPaused);
+    console.log(document.getElementbyId('audiotag1').duration())
+  }
+}
