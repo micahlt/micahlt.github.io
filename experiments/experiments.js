5460d494467f27bd2cacde117d2ac4f470531d57
Micah Lindley <micahincorporated@gmail.com>
2019-12-06 13:15:25 -0600

    Javascript for Experiments landing page

---
+++ experiments/experiments.js
@@ -0,0 +1,11 @@
+var i = 0;
+var output = "<ul>";
+var project = ['<s>Youtube Music Clone</s>', 'GetThatDesc!', '<s>Checkmark</s>', 'CSS Transforms'];
+var linksList = ['youtubemusicclone.html', 'getthatdisc.html', 'checkmark.html', 'csstransforms.html'];
+while (i < project.length) {
+  output = output + "<li><a href = '" + linksList[i] + "'>" + project[i] + "</a></li>";
+  i++;
+};
+output = output + "</ul>";
+console.log("Injecting the following HTML to the projects list: \n\n" + output);
+document.getElementById('exList').innerHTML = output;
