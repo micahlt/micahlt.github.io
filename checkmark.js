a23247126953d04ddd96b210103b7c33b010bd40
Micah Lindley <micahincorporated@gmail.com>
2019-12-06 13:50:42 -0600

    Create checkmark.js

---
+++ checkmark.js
@@ -0,0 +1,51 @@
+function get_todos() {
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str); 
+    }
+    return todos;
+}
+ 
+function add() {
+    var task = document.getElementById('task').value;
+ 
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+ 
+    show();
+ 
+    return false;
+}
+ 
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos));
+ 
+    show();
+ 
+    return false;
+}
+ 
+function show() {
+    var todos = get_todos();
+ 
+    var html = '<ul>';
+    for(var i=0; i<todos.length; i++) {
+        html += '<button class = "waves-effect waves-light btn remove" id="' + i  + '"><li>' + todos[i] + '<i class = "material-icons right">done_outline</i></button></li><br><br></a>';
+    };
+    html += '</ul>';
+ 
+    document.getElementById('todos').innerHTML = html;
+ 
+    var buttons = document.getElementsByClassName('remove');
+    for (var i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
+}
+ 
+document.getElementById('add').addEventListener('click', add);
+show();
