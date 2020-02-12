66ff7c57fefcfc572445c90f24bc585fb96603fa
Micah Lindley <micahincorporated@gmail.com>
2020-02-12 17:25:58 +0000

    Added link to devblog

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var optionTwo = {
   strings: ["Hey, I'm Micah Lindley! ^1000"],
-  typeSpeed: 10
+  typeSpeed: 40
 };
 
 var typedAgain = new Typed('h1', optionTwo);
 
 var options = {
   strings: ['^2000 designer ^1000', 'developer ^1000', 'musician ^1000', ':) ^1000'],
-  typeSpeed: 40
+  typeSpeed: 70
 };
 
 var typed = new Typed('h3', options);
