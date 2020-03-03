51d3d07d18be9dc211e6a1539ca377043bddad72
Micah Lindley <micahincorporated@gmail.com>
2020-03-03 14:52:30 +0000

    A fully revamped website!

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 var optionTwo = {
-  strings: ["Hey, I'm Micah Lindley! ^1000"],
-  typeSpeed: 40
-};
-
-var typedAgain = new Typed('h1', optionTwo);
-
-var options = {
-  strings: ['^2000 designer ^1000', 'developer ^1000', 'musician ^1000', ':) ^1000'],
+  strings: ["^1000", "- Graphic Designer - ^2000", "- Musician - ^2000", "- Blogger - ^2000",  "- Web Developer - ^2000"],
   typeSpeed: 70
 };
+var typedAgain = new Typed('h2', optionTwo);
 
-var typed = new Typed('h3', options);
+function hideModal() {
+  document.getElementById("modal").style.display= "none";
+  document.getElementById("modalBg").style.display = "none";
+  document.getElementById("modal").style.opacity = "0";
+  document.getElementById("modalBg").style.opacity = "0";
+}
+
+function showModal() {
+  document.getElementById("modal").style.display= "block";
+  document.getElementById("modalBg").style.display = "block";
+  document.getElementById("modal").style.opacity = "1";
+  document.getElementById("modalBg").style.opacity = "1";
+}
+
+document.getElementById("modalBg").addEventListener("click", function() {hideModal();});
