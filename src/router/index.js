8fb7d33f37eb545043a7392b5c011f669b5e0531
Micah Lindley <micahincorporated@gmail.com>
2022-03-10 12:59:49 -0600

    Switch to createWebHashHistory for GitHub Pages compatibility

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
   {
@@ -20,7 +20,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
